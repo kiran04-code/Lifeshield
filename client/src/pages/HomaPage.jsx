ec3d5c72681626c5dfe1fe6c84a70f5bf26d9ef7
kiran04-code <kr551344@gmail.com>
2025-07-18 04:33:47 +0530

    day01"

--- client/src/pages/HomaPage.jsx
+++ client/src/pages/HomaPage.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 import HeroSection from '../components/HeroSection'
 import OurService from '../components/OurService'
 import Docter from '../components/Docter'
-import Parents from '../components/parents'
-import Footer from '../components/Footer'
+import Parents from '../components/Parents'
 import WhyPage from '../components/WhyPage'
 
 const HomaPage = () => {
