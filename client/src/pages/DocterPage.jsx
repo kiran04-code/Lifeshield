7a06e84e76e3dee41977b8186d5e69a0db038044
kiran04-code <kr551344@gmail.com>
2025-07-19 12:19:49 +0530

    docter landing page

--- client/src/pages/DocterPage.jsx
+++ client/src/pages/DocterPage.jsx
@@ -6,7 +6,7 @@ import { FaHeartCirclePlus } from "react-icons/fa6";
 import { FaUserDoctor } from "react-icons/fa6";
 const DocterPage = () => {
     return (
-        <div className='w-full bg-[#E0EAFF] h-screen md:flex'>
+        <div className='w-full bg-[#E0EAFF] md:flex'>
             <div>
                 <h1 className="text-[30px] sm:text-2xl md:text-4xl lg:text-6xl font-bold text-[#1057EC] p-5 sm:p-6 md:p-8 lg:p-10 max-w-4xl leading-snug">
                     A trusted space for doctors committed to community health
@@ -27,11 +27,11 @@ const DocterPage = () => {
                 </h1>
 
                 <p className=" italic text-base sm:text-lg md:text-[15px] text-gray-600 px-4 sm:px-6 md:px-8 lg:px-10 py-4 leading-relaxed max-w-3xl ">
-                  -Provide online consultations and manage infant vaccination bookings with ease.  
-  Expand your reach, deliver timely care, and support families anytime, anywhere.
+                    -Provide online consultations and manage infant vaccination bookings with ease.
+                    Expand your reach, deliver timely care, and support families anytime, anywhere.
                 </p>
 
-                <div className='flex md:px-8   py-2'>
+                <div className='flex md:px-8 px-3  py-2'>
                     <div className='  flex gap-5 bg-[#322bff7a] p-6 items-center justify-centere py-2  rounded-xl cursor-pointer  '>
                         <div className='flex justify-between items-center flex-col'>
                             <div className='md:w-10 md:h-10 bg-[#E0EAFF] md:rounded-full md:flex  p-2 rounded-full   border-2 border-[#6552E3] justify-center items-center'>
@@ -48,7 +48,7 @@ const DocterPage = () => {
                     <img
                         src="/School_Productivity_Tracker-removebg-preview.png"
                         alt="Get Vaccinated"
-                        className=" h-110 md:mr-10  max-w-xs sm:max-w-xl md:max-w-md lg:max-w-xl"
+                        className="w-[600px] sm:w-[500px] md:w-full h-auto md:mr-10 object-contain"
                     />
 
                 </div>
