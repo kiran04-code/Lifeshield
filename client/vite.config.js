44593d515db04060471cbce362430eba2371dbde
kiran04-code <kr551344@gmail.com>
2025-08-03 01:42:11 +0530

    done with reWeriting Isuue

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,22 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'public/_redirects',
-          dest: '.' // copied to dist/
-        }
-      ]
-    })
-  ],
-  server: {
-    historyApiFallback: true
-  }
+  plugins: [react(),tailwindcss()],
 })
