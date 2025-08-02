14c90a29c3ddc8017594c1f1b655c3a50bbbf8f0
kiran04-code <kr551344@gmail.com>
2025-08-03 00:23:50 +0530

    onceMore

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/_redirects', // path to your file
+          dest: '.' // copy it to the root of dist/
+        }
+      ]
+    })
+  ]
 })
